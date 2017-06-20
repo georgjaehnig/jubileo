fd7740d742c7858e6e7af38691a6ab4858bcffc6
Georg Jaehnig <georg@jaehnig.org>
2017-06-20 22:37:45 +0200

    Output a timeline of jubilees.

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 'use strict'
 
-var zero = new Date(0);
-
 var born = new Date(1980, 2, 20);
 var today = new Date();
-
-var diff = new Date(today - born);
-
-// var d = new Date();
-// d.setUTCFullYear(1970);
-// d.setUTCMonth(0);
-// d.setDate(1);
-
-var d = zero;
-d.setDate(d.getDate()+20000);
-
-var jubilee = new Date(born.getTime() + d.getTime());
-
-console.log(jubilee.toDateString());
 
 var jubilees = {};
 
 var days = [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,11000,12000,13000,14000,15000,16000,17000,18000,19000,20000];
-
 for (var day of days) {
-	console.log(day);
+	//var period = zero;
+	var jubilee = new Date(born);
+	jubilee.setDate(jubilee.getDate() + day);
+	jubilees[jubilee.getTime()] = day + ' days';
 }
 
-//console.log(diff.getFullYear() - 1970);
-//console.log(zero.getTime());
-//console.log(born.getTime());
+//var jubilees = {};
+var months = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
+for (var month of months) {
+	//var period = zero;
+	var jubilee = new Date(born);
+	jubilee.setMonth(jubilee.getMonth() + month);
+	jubilees[jubilee.getTime()] = month + ' months';
+}
 
+var jubileeTimes = Object.keys(jubilees).sort(function (a, b) {  return a - b;  });
 
+for (var jubileeTime of jubileeTimes) {
+	var jubilee = new Date();
+	jubilee.setTime(jubileeTime);
+	console.log(jubilee.toDateString(), jubilees[jubileeTime]);
+}
