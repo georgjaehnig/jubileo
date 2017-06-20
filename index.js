86e1d7cd8437b0f42d806b4d504ac403b1e3cb36
Georg Jaehnig <georg@jaehnig.org>
2017-06-20 22:50:28 +0200

    Add weeks.

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ for (var day of days) {
 	jubilees[jubilee.getTime()] = day + ' days';
 }
 
+var weeks = [50, 100, 150, 200, 250, 300, 400, 500, 600, 750, 800, 900, 1000,
+						1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,
+						2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,
+						3100,3200,3300,3400,3500,3600,3700,3800,3900,4000];
+for (var week of weeks) {
+	var jubilee = new Date(born);
+	jubilee.setDate(jubilee.getDate() + week * 7);
+	jubilees[jubilee.getTime()] = week + ' weeks';
+}
+
 var months = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 750, 800, 850, 900, 950, 1000];
 for (var month of months) {
 	var jubilee = new Date(born);
