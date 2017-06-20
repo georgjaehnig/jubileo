362dd8986f77a1449199852a4b6830ce90eac4f3
Georg Jaehnig <georg@jaehnig.org>
2017-06-20 22:40:39 +0200

    Cleanup.

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var days = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000,10000,
 	         11000,12000,13000,14000,15000,16000,17000,18000,19000,20000,
 	         21000,22000,23000,24000,25000,26000,27000,28000,29000,30000];
 for (var day of days) {
-	//var period = zero;
 	var jubilee = new Date(born);
 	jubilee.setDate(jubilee.getDate() + day);
 	jubilees[jubilee.getTime()] = day + ' days';
@@ -16,7 +15,6 @@ for (var day of days) {
 
 var months = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 750, 800, 850, 900, 950, 1000];
 for (var month of months) {
-	//var period = zero;
 	var jubilee = new Date(born);
 	jubilee.setMonth(jubilee.getMonth() + month);
 	jubilees[jubilee.getTime()] = month + ' months';
