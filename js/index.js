c2c7ba3d8758750f3cfb59c090922dcf11c8c533
Georg Jaehnig <georg@jaehnig.org>
2017-07-03 22:36:11 +0200

    Update Mime type.

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ END:VEVENT
 // Source: https://stackoverflow.com/a/18197341/52023
 function download(filename, text) {
   var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('href', 'data:text/calendar;charset=utf-8,' + encodeURIComponent(text));
   element.setAttribute('download', filename);
 
   element.style.display = 'none';
